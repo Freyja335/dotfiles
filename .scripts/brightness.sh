6266d9e2b04c7e5ce183fd951840b4ab13075f4d
Freyja335 <92382538+Freyja335@users.noreply.github.com>
2024-10-29 01:13:35 +0200

    fixed brightness.sh script

--- .scripts/brightness.sh
+++ .scripts/brightness.sh
@@ -3,7 +3,6 @@
 # Use brightnessctl to naturally adjust laptop screen brightness and send a notification
 
 currentbrightness=$(brightnessctl -m | awk -F, '{print substr($4, 0, length($4)-1)}')
-if [ "$currentbrightness" -lt 0 ] && [ "$1" = "down" ]; then exit 1; fi
 
 send_notification() {
 	brightness=$(brightnessctl -m | awk -F, '{print substr($4, 0, length($4)-1)}')
